15e17167f8e7fca40e05e23083cc8de20ca69df3
springkyu <seika.k.r0610@gmail.com>
2021-07-22 05:18:01 +0000

    [UPDATE]4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+　has_many: books, dependent: :destroy
 end
