93e9779e75f570fdee2d1856b0b66dc1ee1e7cd3
springkyu <seika.k.r0610@gmail.com>
2021-07-22 08:30:17 +0000

    [UPDATE]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  resources :books
 end
