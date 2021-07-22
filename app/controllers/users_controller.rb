cc3b18278ae54621d6c5cb8cdb7fc5ad41989e75
springkyu <seika.k.r0610@gmail.com>
2021-07-22 14:21:04 +0000

    [UPDATE]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books(params[])
+    @books = @user.books(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 end
