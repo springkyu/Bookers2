e08d8cd2465a7dd508da03cfb3ccc0bb6929c651
springkyu <seika.k.r0610@gmail.com>
2021-07-23 13:46:23 +0000

    [UPDATE]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
     @users = User.all
     @book = Book.new
+    @books = Book.all
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.all
+    @books = Book.where(user_id: @user.id)
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice]="You have updated user success"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
 
