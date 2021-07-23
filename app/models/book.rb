5a65db2fa58a1a4174546085843b90402e48b61d
springkyu <seika.k.r0610@gmail.com>
2021-07-23 17:17:28 +0000

    [UPDATE]4-10

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
   attachment :profile_image
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 200}
 end
