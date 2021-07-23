b585919dcfd021334ddb6f2c655c8cb34771dc1d
springkyu <seika.k.r0610@gmail.com>
2021-07-23 09:18:46 +0000

    [UPDATE]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,8 +32,8 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
