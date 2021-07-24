de6d75c3c903643f423fe6c084117e969b8d69e3
springkyu <seika.k.r0610@gmail.com>
2021-07-24 10:13:39 +0000

    [テスト後修正]4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :name, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
