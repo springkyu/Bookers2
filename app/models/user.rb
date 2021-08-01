655b279fdb2236dc78309c38f6c9c5652d624044
springkyu <seika.k.r0610@gmail.com>
2021-08-01 06:20:31 +0000

    [UPDATE]応用課題３/コメント機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
 
 end
