5e8a63ac4995a7d1d1cb463203efe9c4a6c068f1
springkyu <seika.k.r0610@gmail.com>
2021-08-01 07:40:57 +0000

    [UPDATE]応用課題３/コメント機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,8 @@ class BooksController < ApplicationController
     @newbook = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
+    
   end
 
   def create
