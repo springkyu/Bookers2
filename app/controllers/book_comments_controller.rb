b88180d64d7887e94e4213c6d3bbbfe6fc4ef51e
springkyu <seika.k.r0610@gmail.com>
2021-08-01 08:01:30 +0000

    [UPDATE]応用課題３/コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comment.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to books_path
   end
 
   def destroy
   end
 
+
+  private
+  def book_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+  
 end
