00fa0ef00cbe61f3f878ef1ed47e5c6390180b14
springkyu <seika.k.r0610@gmail.com>
2021-08-01 04:25:39 +0000

    [UPDATE]応用課題３/いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:id])
+    book = Book.find(params[:book_id])
     favorite = current_user.favorite.new(book_id: book.id)
     favorite.save
     redirect_to books_path
   end
 
   def destroy
-    book = Book.find(params[:id])
+    book = Book.find(params[:book])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to books_path
